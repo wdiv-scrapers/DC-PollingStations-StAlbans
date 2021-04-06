e84be36276ad1d3e066973be1ffbced14e9e8dd5
GeoWill <roper.william@gmail.com>
2021-04-06 10:21:54 +0100

    Update council ID

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from dc_base_scrapers.geojson_scraper import RandomIdGeoJSONScraper
 
 stations_url = "http://inspirewfs.stalbans.gov.uk/INSPIRE/WEBSERVICE/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_Stations&outputFormat=application%2Fvnd.geo%2Bjson"
 districts_url = "http://inspirewfs.stalbans.gov.uk/INSPIRE/WEBSERVICE/wfs.exe?service=WFS&version=2.0.0&request=GetFeature&typeNames=ns%3APolling_Districts&outputFormat=application%2Fvnd.geo%2Bjson"
-council_id = 'E07000240'
+council_id = 'SAL'
 
 
 stations_scraper = RandomIdGeoJSONScraper(stations_url, council_id, 'utf-8', 'stations')
